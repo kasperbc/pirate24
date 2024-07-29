aa9bad27fd5e5e835fe16a67bbf5822adcd6d0a6
Kasper Björkgren <kasper.bjorkgren@gmail.com>
2024-07-29 19:00:34 +0300

    new doors and particles

--- components/door/door.gd
+++ components/door/door.gd
@@ -3,8 +3,5 @@ class_name Door
 
 func open():
 	%Sprite2D.play("open")
-	
-	await get_tree().create_timer(2.3).timeout
-	
-	Utils.create_shaker_and_shake(%Sprite2D, 1.5)
+	Utils.create_shaker_and_shake(%Sprite2D, 2.0)
 	%CollisionShape2D.disabled = true
